adc662e3d6d77b708f41ab9eb5a75194f7aca3f0
zpdh <36316038+zpdh@users.noreply.github.com>
2024-07-12 23:39:54 -0300

    Finished CrossCutting and API

---
+++ Catalog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using Catalog.Application.DTOs;
+using Catalog.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetAllAsync()
+    {
+            var categories = await _categoryService.GetAllAsync();
+            return Ok(categories);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetByIdAsync(int id)
+    {
+        var category = await _categoryService.GetByIdAsync(id);
+        
+        if (category == null) return NotFound();
+        
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CategoryDTO>> PostAsync(CategoryDTO entity)
+    {
+        await _categoryService.AddAsync(entity);
+        return Created();
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<CategoryDTO>> PutAsync(CategoryDTO entity)
+    {
+        await _categoryService.UpdateAsync(entity);
+        return Ok(entity);
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult<CategoryDTO>> DeleteAsync(CategoryDTO entity)
+    {
+        await _categoryService.RemoveAsync(entity);
+        return NoContent();
+    }
+}
--- Catalog.CrossCutting/IoC/DependencyInjection.cs
+++ Catalog.CrossCutting/IoC/DependencyInjection.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.CrossCutting.IoC;
+﻿using Catalog.Application.Interfaces;
+using Catalog.Application.Mappings;
+using Catalog.Application.Services;
+using Catalog.Domain.Repositories;
+using Catalog.Infrastructure.Context;
+using Catalog.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-public class DependencyInjection
+namespace Catalog.CrossCutting.IoC;
+
+public static class DependencyInjection
 {
-    
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connStr = configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<DataContext>(options => options.UseMySql(connStr,
+            ServerVersion.AutoDetect(connStr)));
+
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IProductService, ProductService>();
+        
+        services.AddAutoMapper(typeof(DTOMappingProfile));
+        
+        return services;
+    }
 }
